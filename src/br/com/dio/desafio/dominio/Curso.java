942e8147640083f055b25315bf546b3fa1106fd5
ArmandoJunior00 <armando.jr@sofintech.com.br>
2024-08-18 15:11:21 -0300

    adicao de conteudo

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,28 +1,14 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
-
-    String titulo;
-    String descricao;
+public class Curso extends Conteudo {
     int cargaHoraria;
 
+    @Override
+    public double CalcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
     public Curso() {
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
     }
 
     public int getCargaHoraria() {
@@ -35,7 +21,8 @@ public class Curso {
 
     @Override
     public String toString() {
-        return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
+        return "Curso [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", cargaHoraria=" + cargaHoraria
+                + "]";
     }
 
 }
