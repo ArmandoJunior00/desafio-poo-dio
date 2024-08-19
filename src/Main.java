0cda57f98756f70d4989a296fd7444028f02d47b
ArmandoJunior00 <armando.jr@sofintech.com.br>
2024-08-19 19:49:50 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
@@ -12,29 +14,45 @@ public class Main {
         curso1.setTitulo("Java");
         curso1.setDescricao("Descricao curso Java");
         curso1.setCargaHoraria(8);
-        System.out.println(curso1);
+        // System.out.println(curso1);
 
         Curso curso2 = new Curso();
 
         curso2.setTitulo("Js");
         curso2.setDescricao("Descricao curso Js");
         curso2.setCargaHoraria(4);
-        System.out.println(curso2);
+        // System.out.println(curso2);
 
-        Mentoria mentoria1 = new Mentoria();
+        Mentoria mentoria = new Mentoria();
 
-        mentoria1.setTitulo("Mentoria java");
-        mentoria1.setDescricao("Descricao mentoria java");
-        mentoria1.setData(LocalDate.now());
-        System.out.println(mentoria1);
+        mentoria.setTitulo("Mentoria java");
+        mentoria.setDescricao("Descricao mentoria java");
+        mentoria.setData(LocalDate.now());
+        // System.out.println(mentoria);
 
-        Mentoria mentoria2 = new Mentoria();
-        mentoria2.setTitulo("Mentoria js");
-        mentoria2.setDescricao("descricao mentoria js");
-        mentoria2.setData(LocalDate.now());
-        System.out.println(mentoria2);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java developer");
+        bootcamp.setDescricao("Descricao Bootcamp Java developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
 
-        System.out.println("Encapsulamento e abstração");
-        System.out.println("Teste");
+        // DEV ARMANDO
+        Dev devArmando = new Dev();
+        devArmando.setNome("Armando");
+        devArmando.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos: " + devArmando.getConteudosInscritos());
+        devArmando.progredir();
+        System.out.println("Conteudos concluidos: " + devArmando.getConteudosConcluidos());
+        System.out.println("Conteudos inscritos: " + devArmando.getConteudosInscritos());
+        System.out.println("Conteudos concluidos: " + devArmando.getConteudosConcluidos());
+        devArmando.progredir();
+        System.out.println("XP " + devArmando.calcularTotalXp());
+
+
+        /*
+         * System.out.println("Encapsulamento e abstração");
+         * System.out.println("Teste");
+         */
     }
 }
